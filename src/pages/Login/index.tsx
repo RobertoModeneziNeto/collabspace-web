54863b380b420d7935e3dfeec83e16208abfba76
Roberto Modenezi Neto <futspamslt@gmail.com>
2023-07-31 22:45:38 -0300

    Initial commit

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,25 @@
+import { Button, Container, Form, Input, Label } from "./styles";
+
+const Login: React.FC = () => {
+  return (
+    <Container>
+      <Form>
+        <h1>Entrar</h1>
+
+        <div>
+          <Label htmlFor=""> Endereço de e-mail </Label>
+          <Input type="text" placeholder="Digite seu e-email" />
+        </div>
+
+        <div>
+          <Label htmlFor=""> Sua senha secreta </Label>
+          <Input type="text" placeholder="Digite sua senha" />
+        </div>
+
+        <Button>Fazer login</Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Login;
---
+++ src/routes/deauth.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+
+function DeauthRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="*" element={<h1>Not Found!</h1>} />
+    </Routes>
+  );
+}
+
+export default DeauthRoutes;
