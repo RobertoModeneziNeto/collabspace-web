123232cfd86d9cebae732c2919261ed774d0acdb
Roberto Modenezi Neto <futspamslt@gmail.com>
2023-08-08 22:28:02 -0300

    Refactor code

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,23 +1,26 @@
 import { Trash } from "phosphor-react";
 
 import Avatar from "../Avatar";
-import { AuthorAndTime, CommentBox, Container } from "./styles";
+import { AuthorAndTime, ButtonDelete, CommentBox, Container } from "./styles";
 
 const Comment: React.FC = () => {
   return (
     <Container>
-      <Avatar />
+      <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLxpmmwZWwQ0F1olKT3br8rWaliSaHNPvPDg&usqp=CAU" />
 
       <CommentBox>
         <AuthorAndTime>
-          <h1>Roberto Neto</h1>
+          <h1>Rodrigo Goes</h1>
           <time>Cerca de 2h</time>
-          <button>
+          <ButtonDelete>
             <Trash size={22} />
-          </button>
+          </ButtonDelete>
         </AuthorAndTime>
 
-        <p>E esse Baseballbet bem astheric!! já tenho o meu veredito!</p>
+        <p>
+          Vamos analisar esse Baseballbet, me parece bem astheric!! já tenho o
+          meu veredito!
+        </p>
       </CommentBox>
     </Container>
   );
